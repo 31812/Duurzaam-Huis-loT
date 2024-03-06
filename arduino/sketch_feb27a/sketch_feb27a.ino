ea4548906dbb031b2abe6008a8937afd2c2a8007
31812 <31812@ma-web.nl>
2024-03-06 10:20:22 +0100

    opstart

---
+++ arduino/sketch_feb27a/sketch_feb27a.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(D3, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(D3, HIGH);
+  delay(1000);
+  digitalWrite(D3, LOW);
+  delay(2000);
+}
